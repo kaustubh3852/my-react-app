732dbf420b57b7cea17e4f4955c5cfeef68701de
Kaustubh Raj <manman3852@gmail.com>
2026-02-01 23:18:05 +0530

    HTML code

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,18 @@
-import {createRoot} from 'react-dom/client'
+import {createRoot} from 'react-dom/client';
+const myelement = (
+  <table>
+    <tr>
+      <th>name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
 createRoot(document.getElementById('root')).render(
-  <h1>Hello World</h1>
+  myelement
 )
