ae16fbbe7b1dd04d09b6fe9b6ea043df6c885026
Kaustubh Raj <manman3852@gmail.com>
2026-02-02 14:59:53 +0530

    The Root Node

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import {createRoot} from 'react-dom/client';
-createRoot(document.getElementById('sandy')).render(
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('sandy')).render (
   <p>Welcome</p>
 )
