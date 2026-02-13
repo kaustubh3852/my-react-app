9650e0a9a7b33a0baea8f45a41fe36812d6f6e4d
Kaustubh Raj <manman3852@gmail.com>
2026-02-13 16:57:46 +0530

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 const App = () => {
-  const numbers = [1,2,3,4];
-  const doubled = numbers.map(x => x * 2);
-
-  return(
-    <>
-    {doubled.join(",")}
-    </>
+  const fruitlist = ['apple', 'banana', 'cherry'];
+  return (
+    <ul>
+      {fruitlist.map(fruit => 
+        <li key = {fruit}>{fruit}</li>
+      )}
+    </ul>
   );
 }
 
