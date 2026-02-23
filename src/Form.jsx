62080f1dfcb802f90b1fe7022f412ab51cd3a8c3
Kaustubh Raj <manman3852@gmail.com>
2026-02-23 15:59:39 +0530

    submit forms

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,25 @@
 import {useState} from 'react';
 const Form = () => {
     const [name, setName] = useState("");
+
+    function handlechange(e) {
+        setName(e.target.value);
+    }
+
+    function handlesubmit(e) {
+        e.preventDefault();
+        alert(name);
+    }
+
     return (
         <>
-        <form >
-            <label> Enter your Name: </label>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-            <p>Current Value: {name}</p>
+        <form onSubmit={handlesubmit}>
+            <label>Enter your name: </label>
+            <input type="text" value={name} onChange={handlechange} />
+            <input type="submit" />
         </form>
         </>
     );
 }
+
 export default Form;
